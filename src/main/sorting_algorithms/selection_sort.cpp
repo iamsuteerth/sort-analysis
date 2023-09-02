37652746c1168a36a43658eb9ee9f556868f497c
Suteerth Subramaniam <98741486+iamsuteerth@users.noreply.github.com>
2023-09-02 23:53:10 +0530

    Create selection_sort.cpp

---
+++ src/main/sorting_algorithms/selection_sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#inlcude <utility>
+void selection_sort(vector<int> &array){
+    int n = array.size();
+    int min_idx;
+    for(int i = 0 ; i < n ; i++){
+            min_idx = i;
+            for(int j = i + 1 ; j < n ; j++){
+                if(array[j] < array[min_idx])
+                    min_idx = j;
+            }
+            if(min_idx != i)
+                swap(array[min_idx], array[i]);
+    }
+}
