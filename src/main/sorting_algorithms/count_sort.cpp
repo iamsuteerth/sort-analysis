384d153f4410bde1a99fba4ee99478cac5af6d89
Suteerth Subramaniam <98741486+iamsuteerth@users.noreply.github.com>
2023-09-03 23:55:25 +0530

    Create count_sort.cpp

---
+++ src/main/sorting_algorithms/count_sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void countSort(vector<int> &arr){
+    int n = arr.size();
+    int max = max_element(arr.begin(), arr.end());
+    vector<int> count(max + 1, 0);
+    for (int i = 0; i < arr.size(); i++) {
+          count[arr[i]]++;
+    }
+    int index = 0;
+    for (int i = 0; i <= max_element; i++) {
+      while (count[i] > 0) {
+              arr[index] = i;
+              index++;
+              count[i]--;
+          }
+      }
