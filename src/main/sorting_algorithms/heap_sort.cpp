dd5cca81129a207cfd0f0a23aca50034f53244be
Viraj Shah <veer15102003@gmail.com>
2023-09-09 11:32:15 +0530

    added heap sort

---
+++ src/main/sorting_algorithms/heap_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void heapify(vector<int> &arr, int n, int root) {
+  int largest = root;
+  int left = 2 * root + 1;
+  int right = 2 * root + 2;
+  if (left < n && arr[left] > arr[largest])
+    largest = left;
+
+  if (right < n && arr[right] > arr[largest])
+    largest = right;
+
+  if (largest != root) {
+    swap(arr[root], arr[largest]);
+    heapify(arr, n, largest);
+  }
+}
+
+void heapSort(vector<int> &arr) {
+  int n = arr.size();
+
+  for (int i = n / 2 - 1; i >= 0; i--)
+    heapify(arr, n, i);
+
+  for (int i = n - 1; i > 0; i--) {
+    swap(arr[0], arr[i]);
+
+    heapify(arr, i, 0);
+  }
+}
